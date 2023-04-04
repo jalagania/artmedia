1da7cb1a4c65ab845be2c519ef0609068d1c402c
jalagania <beka>
2023-04-04 18:32:12 +0400

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import "./Header.css";
+import logoRed from "../assets/logo-red.png";
+import searchIcon from "../assets/icon-search.svg";
+import { useState } from "react";
+
+function Header() {
+  const [activeLink, setActiveLink] = useState("home");
+
+  return (
+    <header>
+      <div className="section-container">
+        <div className="logo-box">
+          <img src={logoRed} alt="logo" className="logo" />
+          <p className="company-name">Artmedia</p>
+        </div>
+        <nav className="navigation">
+          <ul className="nav-links">
+            <li
+              className={activeLink === "home" ? "active" : ""}
+              onClick={() => setActiveLink("home")}
+            >
+              <a href="#home">Home</a>
+            </li>
+            <li
+              className={activeLink === "services" ? "active" : ""}
+              onClick={() => setActiveLink("services")}
+            >
+              <a href="#services">Services</a>
+            </li>
+            <li
+              className={activeLink === "projects" ? "active" : ""}
+              onClick={() => setActiveLink("projects")}
+            >
+              <a href="#projects">Projects</a>
+            </li>
+            <li
+              className={activeLink === "about" ? "active" : ""}
+              onClick={() => setActiveLink("about")}
+            >
+              <a href="#about">About</a>
+            </li>
+            <li
+              className={activeLink === "contact" ? "active" : ""}
+              onClick={() => setActiveLink("contact")}
+            >
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="search-box">
+          <input type="text" placeholder="Search keyword ..." />
+          <button className="btn-search">
+            <img src={searchIcon} alt="search" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
