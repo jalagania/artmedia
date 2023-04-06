ae0a0845ce0f94f460d6ecdf41b68870fdc99d14
jalagania <beka>
2023-04-06 12:24:14 +0400

    added mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,73 @@
 import "./Header.css";
 import logoRed from "../assets/logo-red.png";
+import iconMenu from "../assets/icon-hamburger.svg";
 import searchIcon from "../assets/icon-search.svg";
 import { useState } from "react";
 
 function Header() {
   const [activeLink, setActiveLink] = useState("home");
+  const [showMenu, setShowMenu] = useState(false);
+
+  document.body.style.overflow = showMenu ? "hidden" : "";
+
+  function handleMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  function handleLink(event) {
+    setActiveLink(event.target.textContent.toLowerCase());
+    setShowMenu(false);
+  }
 
   return (
     <header>
       <div className="section-container" id="home">
+        <button className="btn-menu" onClick={handleMenu}>
+          <img src={iconMenu} alt="menu" />
+        </button>
         <div className="logo-box">
           <img src={logoRed} alt="logo" className="logo" />
           <p className="company-name">Artmedia</p>
         </div>
-        <nav className="navigation">
+        <nav className={`navigation ${showMenu ? "slide-left" : ""}`}>
           <ul className="nav-links">
             <li
               className={activeLink === "home" ? "active" : ""}
-              onClick={() => setActiveLink("home")}
+              onClick={handleLink}
             >
               <a href="#home">Home</a>
             </li>
             <li
               className={activeLink === "services" ? "active" : ""}
-              onClick={() => setActiveLink("services")}
+              onClick={handleLink}
             >
               <a href="#services">Services</a>
             </li>
             <li
               className={activeLink === "projects" ? "active" : ""}
-              onClick={() => setActiveLink("projects")}
+              onClick={handleLink}
             >
               <a href="#projects">Projects</a>
             </li>
             <li
               className={activeLink === "about" ? "active" : ""}
-              onClick={() => setActiveLink("about")}
+              onClick={handleLink}
             >
               <a href="#about">About</a>
             </li>
             <li
               className={activeLink === "contact" ? "active" : ""}
-              onClick={() => setActiveLink("contact")}
+              onClick={handleLink}
             >
               <a href="#contact">Contact</a>
             </li>
           </ul>
+          {showMenu && (
+            <div
+              className="menu-modal-bg"
+              onClick={() => setShowMenu(false)}
+            ></div>
+          )}
         </nav>
         <form className="search-box" onSubmit={(e) => e.preventDefault()}>
           <input type="text" placeholder="Search keyword ..." />
